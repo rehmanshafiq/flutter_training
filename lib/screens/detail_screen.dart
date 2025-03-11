66f05770c14bec50156a075d75143dda2610df0e
Shafiq <shafiq.rehman@nextgeni.com>
2025-03-11 15:30:38 +0500

    Image picker implemented.

--- lib/screens/detail_screen.dart
+++ lib/screens/detail_screen.dart
@@ -1,16 +1,74 @@
+import 'dart:io';
 import 'package:flutter/material.dart';
-import 'package:flutter_training/utils/app_strings.dart';
-import 'package:flutter_training/widgets/text_widget.dart';
+import 'package:image_picker/image_picker.dart';
 
-class DetailScreen extends StatelessWidget {
+class DetailScreen extends StatefulWidget {
+  DetailScreen({Key? key}) : super(key: key);
 
-  const DetailScreen({Key? key}) : super(key: key);
+  @override
+  State<DetailScreen> createState() => _DetailScreenState();
+}
+
+class _DetailScreenState extends State<DetailScreen> {
+
+  bool loading = false;
+  File? selectedImage;
+  String? fileName;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-          child: MyText(AppStrings.detailScreen, style: Theme.of(context).textTheme.displayMedium)
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.onInverseSurface,
+        title: const Text("Multipart api call", style: TextStyle(color: Colors.black),),
+      ),
+      body: Padding(
+          padding: const EdgeInsets.all(20),
+          child: Column(
+            children: [
+              GestureDetector(
+                onTap: () async {
+                  ImagePicker imagePicker = ImagePicker();
+                  XFile? file = await imagePicker.pickImage(source: ImageSource.gallery);
+                  if(file != null) {
+                    selectedImage = File(file.path);
+                    fileName = file.name;
+                    setState(() {});
+                  }
+                },
+                child: selectedImage == null ? Container(
+                  height: 200,
+                  margin: const EdgeInsets.only(bottom: 50),
+                  alignment: Alignment.center,
+                  decoration: BoxDecoration(
+                    color: Colors.grey[300],
+                    borderRadius: BorderRadius.circular(12),
+                  ),
+                  child: Text('Select image'),
+                ): Container(
+                  height: 200,
+                  margin: const EdgeInsets.only(bottom: 50),
+                  alignment: Alignment.center,
+                  decoration: BoxDecoration(
+                    color: Colors.grey[300],
+                    borderRadius: BorderRadius.circular(12),
+                    image: DecorationImage(
+                        fit: BoxFit.cover,
+                        image: FileImage(selectedImage!)
+                    )
+                  ),
+                )
+              ),
+              SizedBox(
+                height: 50,
+                width: double.infinity,
+                child: ElevatedButton(
+                    onPressed: () {},
+                    child: loading? const CircularProgressIndicator(): Text('Upload'),
+                ),
+              )
+            ],
+          ),
       ),
     );
   }
